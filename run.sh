3b49ec11148d530e87baa3aa9b9bb0caff17e9c2
Jens Reidel <adrian@travitia.xyz>
2023-10-22 00:14:51 +0300

    Update
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 podman build -t zlib-ng:latest --no-cache .
 podman create --name zlib-ng zlib-ng:latest
 # /lib
-podman cp zlib-ng:/zlib-ng/libz.so.1.2.12.zlib-ng .
+podman cp zlib-ng:/zlib-ng/libz.so.1.3.0.zlib-ng .
 podman cp zlib-ng:/zlib-ng/libz.a .
 rm libz.so libz.so.1
-ln -s libz.so.1.2.12.zlib-ng libz.so.1
+ln -s libz.so.1.3.0.zlib-ng libz.so.1
 ln -s libz.so.1 libz.so
 # /usr/include
 podman cp zlib-ng:/zlib-ng/zconf.h .
@@ -14,9 +14,9 @@ podman cp zlib-ng:/zlib-ng/zlib_name_mangling.h .
 # /usr/lib/pkgconfig
 podman cp zlib-ng:/zlib-ng/zlib.pc .
 
-podman cp zlib-ng:/lib/libz.so.1.2.12 .
+podman cp zlib-ng:/lib/libz.so.1.2.13 .
 
 podman rm zlib-ng
 
-strip libz.so.1.2.12.zlib-ng
+strip libz.so.1.3.0.zlib-ng
 strip libz.a
